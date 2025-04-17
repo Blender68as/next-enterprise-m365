888a0e52a6ece1ab0013bbb6b94c7305630f447d
Alexander Springer <33805769+AlexanderSpringer@users.noreply.github.com>
2025-04-17 18:17:16 +0200

    fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://yourdomain.de',
+    siteUrl: 'https://behördedigital.de',
     generateRobotsTxt: true,
     exclude: ['/impressum', '/datenschutz'],
     robotsTxtOptions: {
